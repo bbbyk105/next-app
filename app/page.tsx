3ed76ede92989d76dcc445eda6d31f314335fe67
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-08-28 15:52:11 +0900

    初期設定

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello Portfolio</h1>
+    <main
+      className="relative bg-black-100 flex 
+    justify-center items-center flex-col overflow-hidden
+    mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <h1>Hello Portfolio</h1>
+      </div>
     </main>
   );
 }
