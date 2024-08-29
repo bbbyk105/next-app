ea12468714a2879fb2dc81d180254e89c1062de5
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-08-29 21:39:46 +0900

    レイアウトの完成

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -42,16 +42,17 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
         className
       )}
       style={{
-        background: 'rgb(2,0,36)',
-        backgroundColor: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(92,92,105,1) 0%, rgba(204,205,217,1) 26%, rgba(116,138,148,1) 100%)',
+        background: "rgb(4,7,29)",
+        backgroundImage:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-      <div className={`relative ${id === 6 ? 'flex justify-center h-full w-full' : ''}`}>
-        <div className="absolute">
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
+        <div className="w-full h-full absolute">
           {img && (
             <img
               src={img}
@@ -60,27 +61,34 @@ export const BentoGridItem = ({
             />
           )}
         </div>
-        <div className={`absolute right-0 -bottom-5 ${id === 5 ? '' : ''}`}>
-          {spareImg && <img src={spareImg} alt={spareImg} className="" />}
+        <div
+          className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"}`}
+        >
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              className="object-cover object-center w-full h-full"
+            />
+          )}
         </div>
         {id === 6 && (
           <BackgroundGradientAnimation>
-            <div className="absolute z-50 flex items-center justify-center text-white font-bold"></div>
+            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
           </BackgroundGradientAnimation>
         )}
 
-        <div className={cn(
-          titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md'
-        )}>
-          <div />
-        </div>
-
-        <div className="group-hover/bento:translate-x-2 transition duration-200">
-          <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-            {title}
-          </div>
-          <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+        <div
+          className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+          )}
+        >
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
+          </div>
+          <div className="font-sans text-lg lg:text-3xl max-w-96 font-bold z-10">
+            {title}
           </div>
         </div>
       </div>
