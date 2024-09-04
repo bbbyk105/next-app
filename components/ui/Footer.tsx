2cdddd5bd661eb49b446d037baea59a8b981fe4a
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-04 10:43:45 +0900

    linkの追加とiconの挿入

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { div } from "three/webgpu";
+import MagicButton from "./MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data";
+import { url } from "inspector";
 
 const Footer = () => {
   return (
@@ -13,11 +17,36 @@ const Footer = () => {
       </div>
 
       <div className="flex flex-col items-center">
-        <h1>Ready to take 
-            <span className="text-purple">your</span> digital
-            presence to the next level?
+        <h1 className="heading lg:max-w-[45vh]">
+          Are you ready to elevate <span className="text-purple">your</span>{" "}
+          digital presence to new heights?
         </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center ">
+          Contact me today, and let&apos;s talk about how I can assist you in
+          reaching your goals.
+        </p>
+        <a href="bbbyk105@gmail.com">
+          <MagicButton
+            title="Contact me"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
       </div>
+
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Byakko
+        </p>
+      </div>
+
+      <div className="flex items-center justify-end md:gap-3 gap-6">
+            {socialMedia.map((profile) => (
+        <a key={profile.id} href={profile.url} target="_blank" rel="noopener noreferrer" className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
+            <img src={profile.img} alt={`Profile image for ${profile.id}`} width={20} height={20} />
+        </a>
+          ))}
+        </div>
     </footer>
   );
 };
