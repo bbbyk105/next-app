b9e30fac870ab599c28db2e291f0d98d45304361
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-04 18:29:08 +0900

    よく分からないエラー

--- sentry.server.config.ts
+++
@@ -1,7 +0,0 @@
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: "https://178ff6485ea59d6b4cf58026c9380c69@o4507888883859456.ingest.us.sentry.io/4507888887201792",
-  tracesSampleRate: 1,
-  debug: false,
-});
