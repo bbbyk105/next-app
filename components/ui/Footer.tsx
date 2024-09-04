482ee634a85eb58ea47ec99f543edc602c11c9ef
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-04 11:06:58 +0900

    完成間近

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,21 +1,11 @@
 import React from "react";
-import { div } from "three/webgpu";
 import MagicButton from "./MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
 import { socialMedia } from "@/data";
-import { url } from "inspector";
 
 const Footer = () => {
   return (
-    <footer className='w-full pt-20 pb-10 id="contact'>
-      <div className="w-full absolute left-0 -bottom72">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50"
-        />
-      </div>
-
+    <footer className='w-full mb-[100px] pb-10' id="contact">
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vh]">
           Are you ready to elevate <span className="text-purple">your</span>{" "}
@@ -25,7 +15,7 @@ const Footer = () => {
           Contact me today, and let&apos;s talk about how I can assist you in
           reaching your goals.
         </p>
-        <a href="bbbyk105@gmail.com">
+        <a href="mailto:bbbyk105@gmail.com">
           <MagicButton
             title="Contact me"
             icon={<FaLocationArrow />}
@@ -40,13 +30,26 @@ const Footer = () => {
         </p>
       </div>
 
-      <div className="flex items-center justify-end md:gap-3 gap-6">
-            {socialMedia.map((profile) => (
-        <a key={profile.id} href={profile.url} target="_blank" rel="noopener noreferrer" className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
-            <img src={profile.img} alt={`Profile image for ${profile.id}`} width={20} height={20} />
-        </a>
+      <div className="flex flex-col items-center justify-center mt-4 md:mt-0">
+        <div className="flex items-center justify-center md:gap-3 gap-6">
+          {socialMedia.map((profile) => (
+            <a
+              key={profile.id}
+              href={profile.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img
+                src={profile.img}
+                alt={`Profile image for ${profile.id}`}
+                width={20}
+                height={20}
+              />
+            </a>
           ))}
         </div>
+      </div>
     </footer>
   );
 };
