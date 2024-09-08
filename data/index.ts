c15281027a5b304c04fca5e73ad192171b5bbe4a
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-09 00:57:37 +0900

    ai code reviewer

--- data/index.ts
+++ data/index.ts
@@ -97,11 +97,11 @@ export const projects = [
   },
   {
     id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    title: "AI Code Reviewer",
+    des: "OpenAIのAPIを利用してAI Code Reviewerを作成しました",
     img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "https://github.com/adrianhajdin/iphone",
+    iconLists: ["/re.svg", "/tail.svg"],
+    link: "https://ai-code-reviewerjj.netlify.app/",
   },
 ];
 
